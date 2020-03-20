ee9bf74e322c579e21307768dcb64e03cacbd24a
shawn <shawnpatel196@gmail.com>
2020-03-20 10:16:01 -0500

    finished skeleton

---
+++ api/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const apiRouter= require('./api-router.js')
+const server = express();
+
+server.use(express.json());
+server.use('/api', apiRouter)
+
+server.get('/', (req, res)=>{
+    res.status(200).json({api:'up'})
+})
+
+module.exports = server
---
+++ knexfile.js
@@ -0,0 +1,40 @@
+// Update with your config settings.
+
+module.exports = {
+
+  
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/projects.db3'
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    useNullAsDefault: true,
+    pool: {
+        afterCreate: (conn, done) => {
+          conn.run('PRAGMA foreign_keys = ON', done);  }
+      }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
